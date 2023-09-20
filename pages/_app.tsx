dbca679670ea6658cc75996e9c7ce49d9b7f5a36
iqbaltz <mhdiqbaltan@gmail.com>
2023-09-20 16:36:26 +0700

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { Layout } from "@/components/common/layout";
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} className />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} className />
+      </Layout>
+      <Analytics />
+    </>
   );
 }
